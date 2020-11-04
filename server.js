55a67983c1561bcfdfe08f73dd63bec5bb451ce1
kholoudmohtaseb <kholoud.almohtseb@gmail.com>
2020-11-04 14:07:04 +0200

    challenge3

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 
+const express = require("express");
+const app = express();
+const port = 8080;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
+app.use(express.static(__dirname + '/client'));
 
